9645967baf4ac817077fae9e67b7d2ab03f0608a
Megalon <megalongaming@gmail.com>
2021-11-09 13:47:37 -0800

    Add support for Online stage select

--- NickSeriesLogosMod/Patches/StageSelectScreen_MenuOpen.cs
+++ NickSeriesLogosMod/Patches/StageSelectScreen_MenuOpen.cs
@@ -12,60 +12,78 @@ namespace NickSeriesLogosMod.Patches
     [HarmonyPatch(typeof(StageSelectScreen), "MenuOpen")]
     class StageSelectScreen_MenuOpen
     {
+        private static GameObject showLogoObj;
         static void Postfix(StageSelectScreen __instance)
         {
-            var stages = __instance.gameObject.transform.Find("Canvas/MainContainer/LargePreviewLocal/StageMask/RenderVisualizer/Stages");
+            string stagesHierarchyPathLocal  = "Canvas/MainContainer/LargePreviewLocal/StageMask/RenderVisualizer/Stages";
+            string stagesHierarchyPathOnline = "Canvas/MainContainer/LargePreviewOnline/StageMask/RenderVisualizer/Stages";
 
-            if (stages != null)
+            float scale = 0.125f;
+            float x = 568;
+            float ylocal = 791;
+            float yonline = 550;
+
+            var stagesLocal  = __instance.gameObject.transform.Find(stagesHierarchyPathLocal);
+            var stagesOnline = __instance.gameObject.transform.Find(stagesHierarchyPathOnline);
+
+            if (stagesLocal == null)
             {
-                GameObject showLogoObj = new GameObject("Show Logo");
-                RectTransform rectTransform = showLogoObj.AddComponent<RectTransform>();
-
-                rectTransform.anchorMin = Vector2.zero;
-                rectTransform.anchorMax = Vector2.one;
-                rectTransform.sizeDelta = Vector2.zero;
-                rectTransform.localScale = new Vector3(0.125f, 0.125f, 0.125f);
-                rectTransform.anchoredPosition = new Vector2(568, 791);
-
-                showLogoObj.AddComponent<CanvasRenderer>();
-                showLogoObj.AddComponent<Image>();
-
-                // Attach image to each child object
-                for (int i = 0; i < stages.childCount; ++i)
-                {
-                    Transform stage = stages.GetChild(i);
-                    RenderImage renderImage = stage.GetComponent<RenderImage>();
-                    var showId = renderImage.StageMetaData.showId;
-
-                    // Skip empty string
-                    if (showId == null || showId.Equals(string.Empty)) continue;
-
-                    // Skip if we don't have an image for this id
-                    if (!Plugin.logoSpritesDict.ContainsKey(showId))
-                    {
-                        Plugin.LogWarning($"No image found for show \"{showId}\". Skipping...");
-                        continue;
-                    }
-
-                    try
-                    {
-                        GameObject obj = GameObject.Instantiate(showLogoObj, stage);
-                        Image img = obj.GetComponent<Image>();
-                        img.sprite = Plugin.logoSpritesDict[showId];
-
-                        // 16 is menuUI
-                        obj.layer = 16;
-
-                        Plugin.LogDebug($"Loaded image for show \"{showId}\"");
-                    } catch
-                    {
-                        Debug.LogError($"Could not load image for {showId}!");
-                    }
-                }
+                Plugin.LogError($"Could not find \"{stagesHierarchyPathLocal}\"");
+                return;
             }
-            else
+
+            showLogoObj = new GameObject("Show Logo");
+            RectTransform rectTransform = showLogoObj.AddComponent<RectTransform>();
+
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.sizeDelta = Vector2.zero;
+            rectTransform.localScale = new Vector3(scale, scale, scale);
+            rectTransform.anchoredPosition = new Vector2(x, ylocal);
+
+            showLogoObj.AddComponent<CanvasRenderer>();
+            showLogoObj.AddComponent<Image>();
+
+            PopulateStages(stagesLocal);
+
+            rectTransform.anchoredPosition = new Vector2(x, yonline);
+            PopulateStages(stagesOnline);
+        }
+
+        private static void PopulateStages(Transform stages)
+        {
+            // Attach image to each child object
+            for (int i = 0; i < stages.childCount; ++i)
             {
-                Plugin.LogError("Could not find \"Canvas/MainContainer/LargePreviewLocal/StageMask/RenderVisualizer/Stages\"");
+                Transform stage = stages.GetChild(i);
+                RenderImage renderImage = stage.GetComponent<RenderImage>();
+                var showId = renderImage.StageMetaData.showId;
+
+                // Skip empty string
+                if (showId == null || showId.Equals(string.Empty)) continue;
+
+                // Skip if we don't have an image for this id
+                if (!Plugin.logoSpritesDict.ContainsKey(showId))
+                {
+                    Plugin.LogWarning($"No image found for show \"{showId}\". Skipping...");
+                    continue;
+                }
+
+                try
+                {
+                    GameObject obj = GameObject.Instantiate(showLogoObj, stage);
+                    Image img = obj.GetComponent<Image>();
+                    img.sprite = Plugin.logoSpritesDict[showId];
+
+                    // 16 is menuUI
+                    obj.layer = 16;
+
+                    Plugin.LogDebug($"Loaded image for show \"{showId}\"");
+                }
+                catch
+                {
+                    Plugin.LogError($"Could not load image for {showId}!");
+                }
             }
         }
     }
